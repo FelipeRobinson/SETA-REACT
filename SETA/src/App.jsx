176a7180be2ac43e299c6291ce23a7ce52381ea6
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-11-21 19:39:25 -0300

    Update

--- SETA/src/App.jsx
+++ SETA/src/App.jsx
@@ -2,11 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      /* MONTAR OS CÓDIGOS DO HTML AQUI */
+      <nav class="superior">
+        <img src="./DESIGN/Seta.PNG" alt="Logo" id="logo_2" />
+        <div id="escrito">Site Empregador de Trabalhadores Autonomos</div>
+        <section class="search_fil">
+          <div class="search">
+            <div id="botao_filtro">
+              <i class="fa-solid fa-bars"></i>
+            </div>
+            <input type="text" name="BarraDePesquisa" placeholder="Ex: Cursos de programação" id="BarraDePesquisa" />
+            <div id="botao_search">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </div>
+        </section>
+        <div class="botao_bell">
+            <i class="fa-solid fa-bell"></i>
+        </div>
+        <div class="botao_cart">
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+        <div class="user">
+          <i class="fa-solid fa-user" style="color: #4663E6;"></i>
+        </div>
+      </nav>
     </>
   )
 }
